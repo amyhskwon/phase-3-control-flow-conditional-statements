faffd508a9ce217c22592a94b66016d123e8c304
amyhskwon <amyhskwon@gmail.com>
2023-01-02 14:37:36 +0900

    commit tree bc8a6664567dc49536a20c061ccfff415b798f50

--- spec/control_flow_spec.rb
+++ spec/control_flow_spec.rb
@@ -2,7 +2,6 @@ require_relative 'spec_helper'
 require_relative '../control_flow'
 
 describe '#admin_login' do
-  
   it 'returns "Access granted" if the username is "admin" and the password is "12345"' do
     expect(admin_login('admin', '12345')).to eq('Access granted')
   end
